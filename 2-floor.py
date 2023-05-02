0bcc263923c87516fc9ac1129c876817aff0ab0b
nickssilver <51230602+nickssilver@users.noreply.github.com>
2023-05-02 21:53:43 +0300

    Delete 2-floor.py

--- 2-floor.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Contains a single function that calculates the floor of a number."""
-import math
-
-
-def floor(n: float) -> int:
-    """Returns the floor of a float."""
-    return math.floor(n)
